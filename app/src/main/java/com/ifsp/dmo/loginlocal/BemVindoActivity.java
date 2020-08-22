17fc48bbf0751399a4d1cf1bfc9b34b466d98e51
vinicius.montouro <vinicius.montouro@castgroup.com.br>
2020-08-22 16:18:38 -0300

    Ciclo de Vida

--- app/src/main/java/com/ifsp/dmo/loginlocal/BemVindoActivity.java
+++ app/src/main/java/com/ifsp/dmo/loginlocal/BemVindoActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -31,9 +32,47 @@ public class BemVindoActivity extends AppCompatActivity {
         validarUsuario();
     }
 
+    @Override
+    protected void onStart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onStart()");
+        super.onStart();
+    }
+    @Override
+    protected void onRestart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onRestart()");
+        super.onRestart();
+    }
+    @Override
+    protected void onResume() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onResume()");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onPause()");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onStop()");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onDestroy()")
+        ;super.onDestroy();
+    }
+
     private void validarUsuario() {
         //Observe que a validação se dá com os dados armazenados no arquivo de recursos.
-        if (usuario.equals(getString(R.string.user_default)) && senha.equals(getString(R.string.passwd_default))) {
+        if (usuario.equals(getString(R.string.user_default))
+                && senha.equals(getString(R.string.passwd_default))) {
             mensagemTextView.setText(R.string.bem_vindo_msg);
         } else {
             mensagemTextView.setText(R.string.erro_login_msg);
--- app/src/main/java/com/ifsp/dmo/loginlocal/MainActivity.java
+++ app/src/main/java/com/ifsp/dmo/loginlocal/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         usuarioEditText = findViewById(R.id.edittext_usuario);
@@ -33,6 +35,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         logarButton.setOnClickListener(this);
     }
 
+    @Override
+    protected void onStart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onStart()");
+        super.onStart();
+    }
+    @Override
+    protected void onRestart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onRestart()");
+        super.onRestart();
+    }
+    @Override
+    protected void onResume() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onResume()");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onPause()");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onStop()");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +
+                "| Método : onDestroy()")
+        ;super.onDestroy();
+    }
     @Override
     public void onClick(View view) {
         if(view == logarButton){
